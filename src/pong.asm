de24ecd4ec86c5fc31933235c7bc299d23dc7d78
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2025-01-06 23:16:41 +0200

    defined ball position variables and register initialization

--- src/pong.asm
+++ src/pong.asm
@@ -3,11 +3,23 @@ STACK SEGMENT PARA STACK
 STACK ENDS
 
 DATA SEGMENT PARA 'DATA'
+
+    BALL_X DW 0Ah ; x position ( column ) of the ball
+    BALL_Y DW 0Ah ; y position ( line ) of the ball
+
 DATA ENDS
 
 CODE SEGMENT PARA 'CODE'
 
     MAIN PROC FAR
+    ASSUME CS:CODE,DS:DATA,SS:STACK ;assume as code, data and stack segments
+    PUSH DS                         ;push to the stack the ds segment
+    SUB AX,AX                       ;clean ax register
+    PUSH AX                         ;push ax to the stack
+    MOV AX,DATA                     ;save on the ax register the contents of the data segment
+    MOV DS,AX                       ;save on the ds segment the contents of ax
+    POP AX                          ;release the top item from the stack to the ax register
+    POP AX                          ;release the top item from the stack to the ax register
         
         MOV AH,00h ; set the configuration to video mode
         MOV AL,13h ; choose the video mode
@@ -21,8 +33,8 @@ CODE SEGMENT PARA 'CODE'
         MOV AH,0Ch ; set the function to write graphics pixel
         MOV AL,0Fh ; choose white color for the pixel
         MOV BH,00h ; set the page number
-        MOV CX,0Ah ; set the x position
-        MOV DX,0Ah ; set the y position
+        MOV CX,BALL_X ; set the x position
+        MOV DX,BALL_Y ; set the y position
         INT 10h    ; execute the configuration
 
         RET
